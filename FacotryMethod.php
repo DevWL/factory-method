0b59477327fffbc3242c7534e939db383218d1f7
GlupiJas <w.liszkiewicz@gmail.com>
2016-10-02 05:24:34 +0200

    factory method

---
+++ FacotryMethod.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ *  Abstract Product class
+ */
+abstract class Product{
+
+    /**
+     * @var some properyty
+     */
+    private $_id;
+
+    /**
+     * sets product
+     */
+    public function __construct($id){
+        $this->setId($id);
+    }
+
+    public function setId($id){
+        $this->_id = $id;
+        echo $this->_id . PHP_EOL;    
+    }
+}
+
+/**
+ *  Table
+ */
+class Table extends Product{
+    private $legs = 3;
+}
+
+/**
+ *  Chair
+ */
+class Chair extends Product{
+    private $legs = 4;
+}
+
+/**
+ *  Factory class
+ */
+class ProductFactory{
+
+    /**
+     *  Factory create method
+     *  returns object
+     */
+    public static function create($product, $name){
+        if(!class_exists($product)) throw new Exception("Product class $product was not registered or loaded", 1);
+        return new $product($name);
+    }
+}
+
+/**
+ *  Controller uses Factory to create new objects
+ */
+class ProductConroller{
+
+    /**
+     *  Create new product
+     */
+    public function createProduct($class, $param){
+        return ProductFactory::create($class, $param);
+    }
+}
+
+/**
+ * @example
+ */
+$controller = new ProductConroller();
+$Table = $controller->createProduct('Table', 'table-123DFS');
+$Chair = $controller->createProduct('Chair', 'chair-8268CF');
+// $Something = $controller->create('Something', 'Something-0000');
